eeb09c9131826a50a083a119a714992ef7a727d4
[firstname lastname] <jain.khushi2050@gmail.com>
2022-07-12 15:43:36 +0530

    clone made

---
+++ index.js
@@ -0,0 +1,29 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fuga reiciendis reprehenderit rem animi quidem?',
+        image: "images/slider 2.png"
+    },
+    {
+        name: 'loki',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fuga reiciendis reprehenderit rem animi quidem?',
+        image:"images/slider 1.png"
+    },
+    {
+        name: 'wanda vision',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fuga reiciendis reprehenderit rem animi quidem?',
+        image:"images/slider 3.png"
+    },
+    {
+        name: 'raya and the last dragon',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fuga reiciendis reprehenderit rem animi quidem?',
+        image:"images/slider 4.png"
+    },
+    {
+        name: 'luca',
+        des:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fuga reiciendis reprehenderit rem animi quidem?',
+        image:"images/slider 5.png"
+    }
+];
+
+
